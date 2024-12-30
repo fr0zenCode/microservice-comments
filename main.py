85b5d3679625ca3dbdf583f28102b77bb9c1f9f0
Slava <slavik3001@rambler.ru>
2024-12-31 01:12:45 +0300

    Added code for run uvicorn from pycharm

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from api import comments_router
@@ -5,3 +6,7 @@ from api import comments_router
 
 app_comments = FastAPI()
 app_comments.include_router(comments_router)
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app_comments", reload=True)
