2302aab6a39cc9dfc7e1681391e43435e6642b0f
Slava <slavik3001@rambler.ru>
2025-01-05 01:08:23 +0300

    Added method for finding token in cookies

--- app/services/auth.py
+++ app/services/auth.py
@@ -7,14 +7,18 @@ from fastapi import Request, HTTPException
 class AuthService:
 
     @staticmethod
-    def get_bearer_from_request_header(self, request: Request, header_for_search: str = "Authorization"):
+    def get_bearer_from_request_header(request: Request, header_for_search: str = "Authorization"):
         token = request.headers.get(header_for_search)
         if token and token.startswith("Bearer "):
             return token.split(" ", 1)[1]
         raise HTTPException(status_code=401, detail="Not authorized.")
 
-    def get_token_from_cookie(self, request):
-        ...
+    @staticmethod
+    def get_token_from_cookie(request: Request, alias_for_token: str = "token"):
+        token = request.cookies.get(alias_for_token)
+        if token:
+            return token
+        raise HTTPException(status_code=401, detail="Not authorized.")
 
     def decode_token(self, token):
         ...
