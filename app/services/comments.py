b88f71fe69b03af2bd1d479775f6c411158c81c4
Slava <slavik3001@rambler.ru>
2025-01-06 04:07:55 +0300

    Realized all comments service's methods

--- app/services/comments.py
+++ app/services/comments.py
@@ -2,20 +2,48 @@ from dataclasses import dataclass
 
 from starlette.requests import Request
 
-from app.database.crud import get_comment_crud
+from app.database.repositories.abstract import AbstractCommentsRepository
+from app.services.auth import AuthService
 from app.database.schemas import CommentSchemaAdd
 
 
 @dataclass
 class CommentsService:
 
-    comments_dto = get_comment_crud()
+    auth_service = AuthService()
 
-    # надо сделать механику авторизации
+    async def add_comment(
+            self,
+            comment: CommentSchemaAdd,
+            request: Request,
+            comments_repository: AbstractCommentsRepository
+    ):
 
-    async def add_comment(self, comment: CommentSchemaAdd, request: Request):
-        print(request)
-        await self.comments_dto.add_comment(comment_for_add=comment)
+        self.auth_service.authorize_request(request)
+        new_comment_id = await comments_repository.add_comment(comment=comment)
+        return new_comment_id
+
+    async def delete_comment_by_id(
+            self,
+            request: Request,
+            comment_id: int,
+            comments_repository: AbstractCommentsRepository
+    ):
+        self.auth_service.authorize_request(request)
+        await comments_repository.delete_comment_by_id(comment_id=comment_id)
+
+    @staticmethod
+    async def get_comment_by_id(comment_id: int, comments_repository: AbstractCommentsRepository):
+        comment = await comments_repository.get_comment_by_id(comment_id=comment_id)
+        return comment
+
+    @staticmethod
+    async def get_all_comments_for_post_by_post_id(
+            post_id: int,
+            comments_repository: AbstractCommentsRepository
+    ):
+        comments = await comments_repository.get_all_comments_by_post_id(post_id=post_id)
+        return comments
 
 
 def get_comments_service():
