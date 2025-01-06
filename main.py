53a2976d9d6b2fb48904f887667a7be5846bcf56
Slava <slavik3001@rambler.ru>
2025-01-06 04:06:24 +0300

    Added test router for test api's

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
-from api.handlers import comments_router
-
+from app.api.handlers import comments_router
+from app.tests.test_api import test_router
 
 app_comments = FastAPI()
 app_comments.include_router(comments_router)
+app_comments.include_router(test_router)
 
 
 if __name__ == '__main__':
