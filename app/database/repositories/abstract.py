a047247a2a23c19c9e824e1ddc6d4ba562d51b1b
Slava <slavik3001@rambler.ru>
2025-01-07 07:11:57 +0300

    Added return types for abstract repository

--- app/database/repositories/abstract.py
+++ app/database/repositories/abstract.py
@@ -1,22 +1,22 @@
 from abc import ABC, abstractmethod
 
-from app.database.schemas import CommentSchemaAdd
+from app.database.schemas import CommentSchemaAdd, CommentSchema
 
 
 class AbstractCommentsRepository(ABC):
 
     @abstractmethod
-    async def add_comment(self, comment: CommentSchemaAdd):
+    async def add_comment(self, comment: CommentSchemaAdd) -> int:
         raise NotImplementedError
 
     @abstractmethod
-    async def get_comment_by_id(self, comment_id: int):
+    async def get_comment_by_id(self, comment_id: int) -> CommentSchema:
         raise NotImplementedError
 
     @abstractmethod
-    async def delete_comment_by_id(self, comment_id: int):
+    async def delete_comment_by_id(self, comment_id: int) -> dict:
         raise NotImplementedError
 
     @abstractmethod
-    async def get_all_comments_by_post_id(self, post_id: int):
+    async def get_all_comments_by_post_id(self, post_id: int) -> list[CommentSchema]:
         raise NotImplementedError
